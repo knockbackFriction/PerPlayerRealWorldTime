43e3eefcc4fd0ce0eb4803b1955ff3f8d697bae3
knockbackFriction <234834046+knockbackFriction@users.noreply.github.com>
2025-10-17 11:05:16 +0200

    switched to old spigot api for backward compat etc

--- src/main/java/eu/caec/perplayerrealworldtime/Main.java
+++ src/main/java/eu/caec/perplayerrealworldtime/Main.java
@@ -1,13 +1,9 @@
 package eu.caec.perplayerrealworldtime;
 
-import com.destroystokyo.paper.event.server.ServerTickEndEvent;
 import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
-
-import java.time.Instant;
 
 public final class Main extends JavaPlugin implements Listener {
     public static Main instance;
